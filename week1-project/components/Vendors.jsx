26d6b0f5bb465097f0a693c1f416616913cd2cb0
Kenneth Maeda <kenneth.maeda@uconn.edu>
2025-10-01 00:37:32 -0400

    Re-add week1-project as a regular directory

---
+++ week1-project/components/Vendors.jsx
@@ -0,0 +1,60 @@
+import VendorCard from "./VendorsCard";
+
+const Vendors = () => {
+  return (
+    <div className="VendorsGrid">
+      <VendorCard
+        name="GELATO & Cioccolato"
+        img="/vendors/GELATO-Cioccolato.jpeg"
+        culture="Italian"
+      />
+      <VendorCard
+        name="Haven Hot Chicken"
+        img="/vendors/Haven-Hot.png"
+        culture="American"
+      />
+      <VendorCard
+        name="Farmers Market"
+        img="/vendors/Farmers-Market.png"
+        culture="American"
+      />
+      <VendorCard
+        name="Donut Delight"
+        img="https://imgs.search.brave.com/xybBDRhCySiZduAtuL1hQVrXOPCQ0HQ0BBBsbyi1lcE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zMy1t/ZWRpYTAuZmwueWVs/cGNkbi5jb20vYnBo/b3RvL3MySXZPa01R/VnI2dzFlekxwd1d2/OWcvbC5qcGc"
+        culture="American"
+      />
+      <VendorCard
+        name="Stew Lenards"
+        img="https://imgs.search.brave.com/FQCWiVMiJNETaTKA_zrQISCZIAq8FNEOIe-UztSOMC0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zMy1t/ZWRpYTAuZmwueWVs/cGNkbi5jb20vYnBo/b3RvL2RkR3d2ZDFt/MWZseDhVaE9qWEln/NmcvbC5qcGc"
+        culture="American"
+      />
+      <VendorCard
+        name="Post Pizza"
+        img="https://thepizzapost.com/wp-content/uploads/sb-instagram-feed-images/342651658_741181354154585_1095439463043766501_nlow.jpg"
+        culture="Italian"
+      />
+      <VendorCard
+        name="Sally's Pizza"
+        img="https://imgs.search.brave.com/OCinT-2yhGJSSbjjLUrBkKfFulVrfU0qiOBUuH_9rvQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc3F1YXJlc3Bh/Y2UtY2RuLmNvbS9j/b250ZW50L3YxLzUz/MmRmNmU1ZTRiMGJk/OGVmMDM0ZGUwOS8x/NjM5MDc0MzM2NTQw/LUk5OFlaTjEzRVQ0/V0QyTks5NlJGL1Nh/bGx5J3MrQXBpenph/LTEyLmpwZw"
+        culture="Italian"
+      />
+      <VendorCard
+        name="Modern Pizza"
+        img="https://static.wixstatic.com/media/85140e_7291220aee7a4daea489c7169f33228a~mv2.png/v1/fill/w_2012,h_1301,al_c,q_95,enc_avif,quality_auto/85140e_7291220aee7a4daea489c7169f33228a~mv2.png"
+        culture="Italian"
+      />
+      <VendorCard
+        name="Sobol Bowls"
+        img="https://imgs.search.brave.com/hRNvTyppgfOLu6lWUp7F88JjZM0-Y-716IM0Xric5ek/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zMy1t/ZWRpYTAuZmwueWVs/cGNkbi5jb20vYnBo/b3RvL3FNeS1MNTBX/SFFmSm12VzlMWDky/dEEvbC5qcGc"
+        culture="Brazilian"
+      />
+      <VendorCard
+        name="My Kitchen Cooking Area"
+        img="https://imgs.search.brave.com/PuDM8GhGZZQCsyijaI0y3XAtHk-ufC8LbFunSfsWs-k/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/YmhnLmNvbS90aG1i/L3N2bG9XSkhLNFd0/MGZ1ejM2Tl9YX0ND/ZTZ4cz0vMjgyeDE4/OC9maWx0ZXJzOm5v/X3Vwc2NhbGUoKTpz/dHJpcF9pY2MoKS8y/MDIxMDUxOF9zaXBf/MTE3djFfcHJldmll/dy0xZDQ2M2U5YzVh/ZWE0NjdkOTJmMDM4/MzFjMWI3N2NmOC5q/cGc"
+        culture="I Cook for myself and friends :)"
+      />
+    </div>
+  );
+};
+
+export default Vendors;
